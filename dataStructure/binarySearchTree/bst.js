a5ccb474d681d1a015db260e408a5ac5b952260d
huahongjun <huahongjun@csii.com.cn>
2019-02-20 14:58:18 +0800

    二分搜索树2、队列

--- dataStructure/binarySearchTree/bst.js
+++ dataStructure/binarySearchTree/bst.js
@@ -1,6 +1,6 @@
 /*
 * 二分搜索树特性：
-* 1、所有的左孩子小于其父节点，所有的右孩子大于其父节点
+* 1、左子树的所有节点小于其父节点，右子树的所有节点大于其父节点
 * 2、中序遍历的结果是一个从小到大排列的结果
 * */
 
@@ -46,12 +46,13 @@ function BST() {
             return node;
         }
 
-        this.root=_insert(this.root, data);
+        this.root = _insert(this.root, data);
     };
     /*二分搜索树的前序遍历*/
     this.preOrder = function () {
         /*递归:分：读取当前树的根节点+访问当前根节点的左孩子+访问当前根节点的右孩子，基：当前树为空*/
-        var rst=[];
+        var rst = [];
+
         function _preOrder(node) {
             if (node == null) {
                 return
@@ -60,12 +61,14 @@ function BST() {
             _preOrder(node.left);
             _preOrder(node.right);
         }
+
         _preOrder(this.root);
         return rst;
     };
     /*二分搜索树的中序遍历*/
     this.inOrder = function () {
-        var rst=[];
+        var rst = [];
+
         /*递归:分：读取当前树的根节点+访问当前根节点的左孩子+访问当前根节点的右孩子，基：当前树为空*/
         function _inOrder(node) {
             if (node == null) {
@@ -75,12 +78,14 @@ function BST() {
             rst.push(node.data);
             _inOrder(node.right);
         }
+
         _inOrder(this.root);
         return rst;
     };
     /*二分搜索树的后序遍历*/
     this.postOrder = function () {
-        var rst=[];
+        var rst = [];
+
         /*递归:分：读取当前树的根节点+访问当前根节点的左孩子+访问当前根节点的右孩子，基：当前树为空*/
         function _postOrder(node) {
             if (node == null) {
@@ -90,8 +95,109 @@ function BST() {
             _postOrder(node.right);
             rst.push(node.data);
         }
+
         _postOrder(this.root);
         return rst;
     };
+    /*二分搜索树的层序遍历*/
+    this.levelOrder = function () {
+        /* 实现层序遍历需要借助队列这个数据结构，规则如下：
+        *    1、访问一个节点时，把这个节点入列。
+        *    2、把节点出列操作这个节点数据时，同时如果这个节点有左右节点，把左右节点入列
+        * */
+        var rst = [];
+        var queue = new Queue();
+        queue.enqueue(this.root);
+        while (!queue.isEmpty()) {
+            //出列
+            var cur = queue.dequeue();
+            rst.push(cur.data);
+            //入列
+            if (cur.left != null) {
+                queue.enqueue(cur.left)
+            }
+            if (cur.right != null) {
+                queue.enqueue(cur.right)
+            }
+        }
+        return rst;
+    };
+    /*寻找最小元素*/
+    this.getMin = function () {
+        //根据特性最小节点一定是最左下角的节点
+        var cur = this.root;
+        var prev;
+        while (cur != null) {
+            prev = cur;
+            cur = cur.left
+        }
+        return prev.data;
+    };
+    /*寻找最大元素*/
+    this.getMax = function () {
+        //根据特性，最大元素一定在最右下角
+        var cur = this.root, prev;
+        while (cur != null) {
+            prev = cur;
+            cur = cur.right;
+        }
+        return prev.data;
+    };
+    /*删除树中存在的元素*/
+    this.remove = function (data) {
+        /* 递归思想：
+        *  入：两种种情况，1、当前节点连上，在其左子树移除元素后返回的节点。
+        *             2、当前节点连上，在其左子树移除元素后返回的节点。
+        *  出：两种情况，1、找不到待删除元素。
+        *             2、找到了元素，删除了当前节点。
+        * */
+        // 删除掉以node为根的二分搜索树中值为e的节点, 递归算法
+        // 返回删除节点后新的二分搜索树的根
+        var _this = this;
 
+        function _remove(node, data) {
+            /*出口*/
+            //找不到删除元素
+            if (node == null) {
+                return null
+            }
+            //删除了当前根节点
+            if (node.data === data) {
+                //当前节点没有左子树,当前节点位置由其右节点占据
+                if (node.left == null) {
+                    _this.size--;
+                    return node.right;//返回右子树的根节点，由上一层函数指向
+                }
+                //当前节点没有右子树，当前节点位置由其左节点占据 
+                else if (node.right == null) {
+                    _this.size--;
+                    return node.left;
+                }
+                //当前节点有左右子树，用右子树的最小节点代替
+                else {
+                    var cur = node.right, prev;
+                    while (cur.left != null) {
+                        prev = cur;
+                        cur = cur.left
+                    }
+                    prev.left = null;
+                    _this.size--;
+                    cur.left = node.left;
+                    cur.right = node.right;
+                    return cur;//返回上一层函数
+                }
+            }
+            /*入口*/
+            //当在右子树中删除了节点
+            if (data > node.data) {
+                node.right = _remove(node.right, data);
+                return node;//记得这个函数都要返回修改过后的根节点
+            } else {
+                node.left = _remove(node.left,data);
+                return node;
+            }
+        }
+
+        _remove(_this.root,data);
+    }
 }
---
+++ dataStructure/queue/queue.js
@@ -0,0 +1,22 @@
+/*
+* js的数组是一个动态数组，有着其他语言数组没有的优势，push和shift方法能够轻松实现队列的功能。
+* 为了更直观的使用队列，我们利用push和shift方法定义一个队列的构造函数
+* */
+function Queue() {
+    this.dataSet=[];//维护的数组
+    this.getSize=function () {
+        return this.dataSet.length;
+    };
+    this.enqueue=function (data) {
+        this.dataSet.push(data);
+    };
+    this.dequeue=function () {
+        return this.dataSet.shift();
+    };
+    this.isEmpty=function () {
+        return this.getSize()===0;
+    };
+    this.toString=function () {
+        return dataSet
+    }
+}
